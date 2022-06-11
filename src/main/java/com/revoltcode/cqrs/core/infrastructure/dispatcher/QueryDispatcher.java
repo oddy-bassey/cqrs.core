7ac9a0bbc8132ad518fbd396acd578537e8d690c
oddy-bassey <oddybassey@gmail.com>
2022-06-11 17:31:30 +0100

    defined the query dispatcher interface

---
+++ src/main/java/com/revoltcode/cqrs/core/infrastructure/dispatcher/QueryDispatcher.java
@@ -0,0 +1,11 @@
+package com.revoltcode.cqrs.core.infrastructure.dispatcher;
+import com.revoltcode.cqrs.core.domain.BaseEntity;
+import com.revoltcode.cqrs.core.query.BaseQuery;
+import com.revoltcode.cqrs.core.query.QueryHandlerMethod;
+
+import java.util.List;
+
+public interface QueryDispatcher {
+    <T extends BaseQuery> void registerHandler(Class<T> type, QueryHandlerMethod<T> handler);
+    <U extends BaseEntity> List<U> send(BaseQuery query);
+}
